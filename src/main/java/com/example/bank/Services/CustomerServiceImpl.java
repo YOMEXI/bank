4fad2f508f0cc198fcd04b4b9f2fbaf6ee63ba49
unknown <abayomexy@gmail.com>
2023-04-17 00:01:46 -0700

    home testing

--- src/main/java/com/example/bank/Services/CustomerServiceImpl.java
+++ src/main/java/com/example/bank/Services/CustomerServiceImpl.java
@@ -16,9 +16,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
 
 @Service
 @RequiredArgsConstructor
--- src/test/java/com/example/bank/services/CustomerServicesTest.java
+++ src/test/java/com/example/bank/services/CustomerServicesTest.java
@@ -1,34 +1,109 @@
 package com.example.bank.services;
 
+import com.example.bank.Entities.Customer;
+import com.example.bank.Enums.AccountType;
 import com.example.bank.HelperClasses.AccountMethods;
 import com.example.bank.Mapper.CustomerMapper;
+import com.example.bank.Payload.CustomerDto;
 import com.example.bank.Repository.CustomerRepository;
 import com.example.bank.Repository.TransactionRepository;
 import com.example.bank.Services.CustomerService;
 import com.example.bank.Services.CustomerServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomerServicesTest {
 
+    @Mock
     private CustomerRepository customerRepository;
-    private CustomerService customerService;
 
+    @InjectMocks
+    private CustomerServiceImpl customerService;
+
+    @Mock
     private  TransactionRepository transactionRepository;
+    @Mock
     private CustomerMapper customerMapper;
+    @Mock
     private AccountMethods accountMethods;
+
+    private Customer newCustomer;
+
+    private CustomerDto customerDto;
 
     @BeforeEach
     public void setup(){
-        customerRepository = Mockito.mock(CustomerRepository.class);
-        customerService = new CustomerServiceImpl(customerRepository
-                ,transactionRepository,customerMapper,accountMethods);
+        List transactions = new ArrayList<>();
+         newCustomer = Customer.builder()
+                .age(23)
+                .transactions(transactions)
+                .email("mikel@gmail.com")
+                .accountType(AccountType.current)
+                .balance(0L)
+                .accountName("Zobo Lord")
+                .accountNumber(1111111111L)
+                .age(23)
+                .build();
 
     }
 
     @Test()
     public  void givenCustomerDto_whenSave_thenReturnCreatedAccount(){
+
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setBalance(30000L);
+        customerDto.setEmail("mikel@gmail.com");
+        customerDto.setAge(23);
+        customerDto.setAccountType("current");
+        customerDto.setAccountName("Zobo Lord");
+        customerDto.setTransactions(new ArrayList<>());
+
+
+        Long AccountNumber=   0L;
+
+
+
+
+        given(customerRepository
+                .findByAccountNumber(AccountNumber)).willReturn(Optional.empty());
+
+        given(customerRepository
+                .findByAccountName(customerDto.getAccountName())).willReturn(Optional.empty());
+
+       when(customerMapper.CustomerDtoToCustomer(customerDto)).thenReturn(newCustomer);
+
+
+        Customer customer = customerMapper.CustomerDtoToCustomer(customerDto);
+
+        when(customerMapper.CustomerToCustomerDto(customer)).thenReturn(customerDto);
+        when(customerRepository
+                .save(customer)).thenReturn(newCustomer);
+
+
+
+        var createdCustomer = customerService.createAccount(customerDto);
+
+        System.out.println(createdCustomer);
+        assertThat(createdCustomer.getAccountName()).isEqualTo("Zobo Lord");
+
+
+
+
+        assertThat(createdCustomer).isNotNull();
 
 
     }
